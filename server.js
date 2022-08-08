ced3a12a1fc4aebf0e515361d795e1300c936dcc
Camo282 <camo282@gmail.com>
2022-08-08 18:31:35 -0400

    created query selectors

--- server.js
+++ server.js
@@ -20,15 +20,38 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-  db.query('SELECT * FROM candidates', (err, rows) => {
+  /*db.query('SELECT * FROM candidates', (err, rows) => {
     console.log(rows);
   });
-  
-// Default response for any other request (Not Found)
+  //GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+});
+//Default response for any other request (Not Found)
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
 app.use((req, res) => {
   res.status(404).end();
-});
+});*/
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
 
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
